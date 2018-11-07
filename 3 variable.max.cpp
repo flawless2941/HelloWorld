3d45d0943447a854be8770db2aee803685b69451
flawless0714 <flawless0714@gmail.com>
2018-11-08 03:19:10 +0800

    test

--- 3 variable.max.cpp
+++ 3 variable.max.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <iostream> /* git test */
 #define min(x,y,z) ((a<b ? a:b)<c ? (a<b ?a:b) : c) 
 #define  max(a,b,c) ((a>b ? a:b) > c? (a>b ? a:b) :c )
 /*test*/
@@ -9,10 +9,18 @@ using namespace std ;
 
 int main(int argc, char** argv) 
 	{
+<<<<<<< HEAD
 	cout<<"�п�J�T�ӼơG"<<endl;
 	int a,b,c ;
 	cin>>a>>b>>c;
 	cout<<"�T�ӼƤ��̤j���Ƭ��G"<<max(a,b,c)<<endl;
 	cout<<"        �̤p���Ƭ��G"<<min(a,b,c)<<endl; 
+=======
+	cout<<"請輸入三個數："<<endl;
+	int a,b,c ;
+	cin>>a>>b>>c;
+	cout<<"三個數中最大的數為："<<max(a,b,c)<<endl;
+	cout<<"        最小的數為："<<min(a,b,c)<<endl; 
+>>>>>>> 184a25946b67146f0827f845fcc88a0383d1c480
 	return 0;
 } 
