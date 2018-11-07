41f1b1d9b95b9401b0c2a1d6f47136ac1657161a
flawless0714 <flawless0714@gmail.com>
2018-11-08 03:25:34 +0800

    test

--- 3 variable.max.cpp
+++ 3 variable.max.cpp
@@ -1,4 +1,4 @@
-#include <iostream> /* git test */
+#include <iostream> /* git test 12222*/
 #define min(x,y,z) ((a<b ? a:b)<c ? (a<b ?a:b) : c) 
 #define  max(a,b,c) ((a>b ? a:b) > c? (a>b ? a:b) :c )
 /*test*/
