855ca1a437669ce17ea9f2ef9ba757d87a726598
Feng Hui-Chun <flawless0714@gmail.com>
2019-04-19 16:55:31 +0800

    Test ref of commit
    please refer to 1c8c90d0e1b008846746ef540a57ba6df87118ea.

--- 3 variable.max.cpp
+++ 3 variable.max.cpp
@@ -2,7 +2,7 @@
 #define min(x,y,z) ((a<b ? a:b)<c ? (a<b ?a:b) : c) 
 #define  max(a,b,c) ((a>b ? a:b) > c? (a>b ? a:b) :c )
 /*test*/
-
+// commit ref test
 using namespace std ;
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
