0004bd49e381389bcbadbe9275b2aaf9b809bd96
flawless0714 <flawless0714@gmail.com>
2019-07-07 02:24:59 +0800

    test done

--- 3 variable.max.cpp
+++ 3 variable.max.cpp
@@ -1,4 +1,4 @@
-#include <iostream> //ssh test commit test`dadsdw
+#include <iostream> //ssh test commit test`dadsdwdwa
 #define min(x,y,z) ((a<b ? a:b)<c ? (a<b ?a:b) : c) 
 #define  max(a,b,c) ((a>b ? a:b) > c? (a>b ? a:b) :c )
 /*test2*/
