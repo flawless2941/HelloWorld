81bebbb264edd735d7a8bf5cc3262bb07eaee679
test2941 <flawless2941@gmail.com>
2019-07-07 19:21:29 +0800

    prepare PR test

--- 3 variable.max.cpp
+++ 3 variable.max.cpp
@@ -1,4 +1,4 @@
-#include <iostream> //ssh test commit test`dadsdwdwa
+#include <iostream> //ssh test commit test`dadsdwdwaawdad
 #define min(x,y,z) ((a<b ? a:b)<c ? (a<b ?a:b) : c) 
 #define  max(a,b,c) ((a>b ? a:b) > c? (a>b ? a:b) :c )
 /*test2*/
